f4640dfefdf97e1fadf8b85434516f1c3f103b56
WaceraMercyThird <waceraaamercy@gmail.com>
2022-03-07 18:02:00 +0300

    add file

---
+++ src/main.kt
@@ -0,0 +1,27 @@
+fun main () {
+    // Addition
+    var a = 5
+    var b = 6
+    println(a + b)
+
+    // Subtraction
+    var x = 19
+    var y = 10
+    println(x - y)
+
+    // concatenate
+    var firstName = "Mary"
+    var secondName = "Johnson"
+    println(firstName + secondName)
+
+
+    var greetings = "Hello, World"
+    println(greetings)
+
+    var name = "Maxine"
+    var age = 35
+    val year = 1986
+    println("My name is" + " " + name + ", I am" + " " + age + " " + "years old" + " " + "and I was in" + " " + year)
+
+
+}
